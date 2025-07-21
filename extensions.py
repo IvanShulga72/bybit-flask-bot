d01f702bf8e13f0473c00c1147b745b8f9db572a
Ivan Shulga <shulga.ivan.dm@gmail.com>
2025-07-21 11:27:49 +0300

    Del dont need py file

--- extensions.py
+++
@@ -1,8 +0,0 @@
-# extensions.py
-from flask import Flask
-from pybit.unified_trading import HTTP
-from keys import api, secret
-
-app = Flask(__name__)
-db_handler = None  # Будет инициализировано позже
-bybit_session = HTTP(testnet=False, api_key=api, api_secret=secret)
